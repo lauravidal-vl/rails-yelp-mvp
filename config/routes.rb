fdca452bed1e903d49ab948b27ce2052490f8688
laura vidal <lauravidal.vl@gmail.com>
2024-02-06 11:52:12 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/:id/reviews/new", to: "reviews#new"
+  # post "restaurants/:id/reviews", to: "reviews#create"
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  # resources :reviews, only: [:show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,45 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+bristol = Restaurant.new
+bristol.save
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 07 08 09 01',
+    category:        "belgian"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '06 07 08 09 02',
+    category:        "italian"
+  },
+  {
+    name:         'Aloy Thai',
+    address:      'Rue Matabiau',
+    phone_number:  '06 07 08 09 09',
+    category:        "chinese"
+  },
+  {
+    name:         'French burger',
+    address:      'Rue de Toulouse',
+    phone_number:  '07 07 08 09 02',
+    category:        "french"
+  },
+  {
+    name:         'Sushi bar',
+    address:      'Rue de Toyko',
+    phone_number:  '06 10 08 09 02',
+    category:        "japanese"
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
